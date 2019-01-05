71ae8ce13880d851136be5163448f99bc8c8dc3f
Bread0288 <42908590+Bread0288@users.noreply.github.com>
2019-01-05 15:44:16 +0900

    ContactList 구현

--- app/src/main/java/com/example/myapplication/ContactsActivity.java
+++ app/src/main/java/com/example/myapplication/ContactsActivity.java
@@ -1,4 +1,169 @@
 package com.example.myapplication;
 
-public class ContactsActivity {
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ListView;
+import java.util.ArrayList;
+import java.util.Collections;
+import org.json.JSONObject;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+public class ContactsActivity extends AppCompatActivity implements ProfileListAdapter.EventListener{
+    public ArrayList<Profile> _profiles_data;
+    public ArrayList<Profile> _profiles_show;
+    private ProfileListAdapter adapter;
+    private EditText listSearch;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacts);
+        _profiles_data = new ArrayList();
+        _profiles_show = new ArrayList();
+        adapter = new ProfileListAdapter(this, this, R.layout.profileview, _profiles_show);
+        ListView listView = findViewById(R.id.numberlist);
+        listView.setAdapter(adapter);
+        final EditText listSearch = (EditText) findViewById(R.id.numlistSearch);
+        listSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+                public void afterTextChanged(Editable s) {
+                String text = listSearch.getText().toString();
+                search(text);
+            }
+        });
+    }
+
+    public void reloadContacts() {
+        _profiles_data.clear();
+        _profiles_show.clear();
+        fetchContacts();
+        adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        reloadContacts();
+    }
+
+    public void fetchContacts() {
+
+        Uri CONTENT_URI = ContactsContract.Contacts.CONTENT_URI;
+        String _ID = ContactsContract.Contacts._ID;
+        String DISPLAY_NAME = ContactsContract.Contacts.DISPLAY_NAME;
+        String HAS_PHONE_NUMBER = ContactsContract.Contacts.HAS_PHONE_NUMBER;
+
+        Uri PhoneCONTENT_URI = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+        String Phone_CONTACT_ID = ContactsContract.CommonDataKinds.Phone.CONTACT_ID;
+        String NUMBER = ContactsContract.CommonDataKinds.Phone.NUMBER;
+
+        Uri EmailCONTENT_URI =  ContactsContract.CommonDataKinds.Email.CONTENT_URI;
+        String EmailCONTACT_ID = ContactsContract.CommonDataKinds.Email.CONTACT_ID;
+        String DATA = ContactsContract.CommonDataKinds.Email.DATA;
+
+        ContentResolver contentResolver = this.getContentResolver();
+
+        Cursor cursor = contentResolver.query(CONTENT_URI, null,null, null, null);
+
+        // Loop for every contact in the phone
+        if (cursor.getCount() > 0) {
+
+            while (cursor.moveToNext()) {
+
+                String contact_id = cursor.getString(cursor.getColumnIndex( _ID ));
+                String name = cursor.getString(cursor.getColumnIndex( DISPLAY_NAME ));
+                String phoneNumber = "";
+                String email = "";
+
+                int hasPhoneNumber = Integer.parseInt(cursor.getString(cursor.getColumnIndex( HAS_PHONE_NUMBER )));
+
+                if (hasPhoneNumber > 0) {
+                    // Query and loop for every phone number of the contact
+                    Cursor phoneCursor = contentResolver.query(PhoneCONTENT_URI, null, Phone_CONTACT_ID + " = ?", new String[] { contact_id }, null);
+
+                    while (phoneCursor.moveToNext()) {
+                        phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(NUMBER));
+                    }
+
+                    phoneCursor.close();
+
+                    // Query and loop for every email of the contact
+                    Cursor emailCursor = contentResolver.query(EmailCONTENT_URI,null, EmailCONTACT_ID+ " = ?", new String[] { contact_id }, null);
+
+                    while (emailCursor.moveToNext()) {
+                        email = emailCursor.getString(emailCursor.getColumnIndex(DATA));
+                    }
+
+                    emailCursor.close();
+                }
+
+                int lookupKeyIndex = cursor.getColumnIndex(ContactsContract.Contacts.LOOKUP_KEY);
+                String currentLookupKey = cursor.getString(lookupKeyIndex);
+                int idIndex = cursor.getColumnIndex(ContactsContract.Contacts._ID);
+                long currentId = cursor.getLong(idIndex);
+                Uri contactUri = ContactsContract.Contacts.getLookupUri(currentId, currentLookupKey);
+
+                // Modify the phone number string to look great!
+                if (phoneNumber.contains("010") && !phoneNumber.contains("-")) {
+                    phoneNumber = new StringBuilder(phoneNumber).insert(3, "-").toString();
+                    phoneNumber = new StringBuilder(phoneNumber).insert(phoneNumber.length()-4, "-").toString();
+                }
+
+                Profile profile = new Profile(name, phoneNumber, email, contactUri);
+                _profiles_data.add(profile);
+            }
+        }
+        Collections.sort(_profiles_data, new CompareProfile());
+        _profiles_show.addAll(_profiles_data);
+    }
+
+    public void search(String targetText) {
+        _profiles_show.clear();
+        targetText = targetText.toLowerCase();
+
+        if (targetText.length() == 0) {
+            _profiles_show.addAll(_profiles_data);
+        }
+        else {
+            for (int i = 0 ; i < _profiles_data.size() ; i++) {
+                if (_profiles_data.get(i).getName().toLowerCase().contains(targetText) ||
+                        _profiles_data.get(i).getPhone().toLowerCase().replace("-","").contains(targetText) ||
+                        _profiles_data.get(i).getEmail().toLowerCase().contains(targetText) ) {
+
+                    _profiles_show.add(_profiles_data.get(i));
+                }
+            }
+        }
+        adapter.notifyDataSetChanged();
+    }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,9 +1,26 @@
 package com.example.myapplication;
 
+import android.Manifest;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
@@ -17,54 +34,94 @@ import com.facebook.login.widget.LoginButton;
 
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private CallbackManager callbackManager;
+    private final int MY_PERMISSIONS_REQUEST = 100;
+    private boolean isPermissionGranted = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //FacebookSdk.sdkInitialize(this.getApplicationContext());
-        setContentView(R.layout.activity_chattingpage);
+        //activity main 레이아웃을 표시
+        setContentView(R.layout.activity_main);
 
-        callbackManager = CallbackManager.Factory.create();
+        //퍼미션 체크 한꺼번에 받음
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
+                            Manifest.permission.READ_CONTACTS,
+                            Manifest.permission.CALL_PHONE,
+                            Manifest.permission.CAMERA,
+                            Manifest.permission.WRITE_CONTACTS,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    MY_PERMISSIONS_REQUEST);
+        }
+        else
+            isPermissionGranted = true;
 
-        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button2);
-        loginButton.setReadPermissions(Arrays.asList("public_profile", "email"));
-        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+//        if (isPermissionGranted) {
+//            processOnCreate();
+//        }
+
+        //Main 화면의 +버튼을 누를 시 새로운 채팅방을 만들기 위해 가지고 있는 전화번호부 목록으로 넘어가는 intent 설정
+        android.support.design.widget.FloatingActionButton fab = findViewById(R.id.chatplus_button);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onSuccess(LoginResult loginResult) {
-                GraphRequest graphRequest = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
-                    @Override
-                    public void onCompleted(JSONObject object, GraphResponse response) {
-                        Log.v("result",object.toString());
-                    }
-                });
-
-                Bundle parameters = new Bundle();
-                parameters.putString("fields", "id,name,email,gender,birthday");
-                graphRequest.setParameters(parameters);
-                graphRequest.executeAsync();
-            }
-
-            @Override
-            public void onCancel() {
-
-            }
-
-            @Override
-            public void onError(FacebookException error) {
-                Log.e("LoginErr",error.toString());
+            public void onClick(View v) {
+                Log.d("Hello", "Intent Start");
+                Intent contactIntent = new Intent(getApplicationContext(), ContactsActivity.class);
+                startActivity(contactIntent);
             }
         });
-    }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        callbackManager.onActivityResult(requestCode, resultCode, data);
+        // facebook 연동
+//        callbackManager = CallbackManager.Factory.create();
+//
+//        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+//        loginButton.setReadPermissions(Arrays.asList("public_profile", "email"));
+//        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+//            @Override
+//            public void onSuccess(LoginResult loginResult) {
+//                GraphRequest graphRequest = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+//                    @Override
+//                    public void onCompleted(JSONObject object, GraphResponse response) {
+//                        Log.v("result",object.toString());
+//                    }
+//                });
+//
+//                Bundle parameters = new Bundle();
+//                parameters.putString("fields", "id,name,email,gender,birthday");
+//                graphRequest.setParameters(parameters);
+//                graphRequest.executeAsync();
+//            }
+//
+//            @Override
+//            public void onCancel() {
+//
+//            }
+//
+//            @Override
+//            public void onError(FacebookException error) {
+//                Log.e("LoginErr",error.toString());
+//            }
+//        });
     }
+//
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        callbackManager.onActivityResult(requestCode, resultCode, data);
+//    }
 }
