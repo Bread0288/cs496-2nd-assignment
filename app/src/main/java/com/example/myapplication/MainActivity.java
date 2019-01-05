3539c4d835108839e91842b4d06a3944f87a86e1
lcbk0321 <lcbk_0321@dgist.ac.kr>
2019-01-05 16:42:28 +0900

    Merge remote-tracking branch 'remotes/origin/master'
    # Conflicts:
    #	.idea/vcs.xml
    #	app/src/main/java/com/example/myapplication/MainActivity.java
    #	app/src/main/java/com/example/myapplication/Retrofit/IMyService.java
    #	app/src/main/java/com/example/myapplication/Retrofit/RetrofitClient.java
    #	app/src/main/res/layout/activity_contacts.xml

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
